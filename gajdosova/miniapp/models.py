251496765ce276946a8d3941a4e43d1a26d05861
Eluna2004 <prahaeliska@seznam.cz>
2024-01-09 11:36:17 +0100

    222

---
+++ gajdosova/miniapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class datainsert(models.Model):
+    title=models.CharField(max_length=200, default="x")
+    name=models.CharField(max_length=200, default="x")
+    play=models.CharField(max_length=200, default="x")
+
+
+class post(models.Model):
+    author=models.CharField(max_length=200, default="x")
+    title=models.CharField(max_length=200, default="x")
+    mood=models.CharField(max_length=200, default="x")
+    amount=models.DecimalField(max_digits=10, decimal_places=2)
+
