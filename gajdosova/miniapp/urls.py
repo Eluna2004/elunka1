369a5ee93e8a61513bd6dddb6e63e5003f74c24f
Eluna2004 <prahaeliska@seznam.cz>
2024-02-27 11:01:50 +0100

    222

--- gajdosova/miniapp/urls.py
+++ gajdosova/miniapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.shortcuts import render
 
 from . import views
 
 urlpatterns = [
-    path('randomnumber/', views.models_list)
+    path('randomnumber/', views.models_list),
+    path('randomnumber_html/', views.post_list)
 ]
+
--- gajdosova/miniapp/views.py
+++ gajdosova/miniapp/views.py
@@ -1,10 +1,11 @@
 import random
-
-from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render
 
 def models_list(reguest):
     random_number = random.randint(1,   100)
     return HttpResponse(f"random number {random_number}")
+
+def post_list(request):
+    random_number = random.randint(1, 100)
+    return render(request, 'miniapp1/post_list.html', {'VAR': random_number})
